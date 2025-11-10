9a452b227b5c5947a87bf01eb3f715a4e578ee45
An Hyeon Seok <123443488+nyeonseok@users.noreply.github.com>
2025-11-10 14:39:30 +0900

    Revert "Feat: 프론트엔드 서버 기본 세팅"

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate", // 자동 업데이트
-      manifest: {
-        name: "STAGE ON",
-        short_name: "StageOn",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#C66CD9",
-        icons: [
-          {
-            src: "/stageon_192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/stageon_512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
-});
+  plugins: [react()],
+})
