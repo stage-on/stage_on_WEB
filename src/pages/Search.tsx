25a4a825a1e77c261516271607e09b0a043dc55d
nyeonseok <aragorn2358@naver.com>
2025-11-11 11:41:30 +0900

    feat: 추천 검색어 클릭 시 입력창에 입력되도록

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -2,13 +2,53 @@ import searchStyle from "../css/pages/search.module.css";
 import searchSVG from "../assets/search/search.svg";
 import menuSVG from "../assets/search/menu.svg";
 import xSVG from "../assets/search/x.svg";
+import { useEffect, useState } from "react";
 
 export default function Search() {
+  const [inputText, setInputText] = useState("");
+  const recommendLista: any[] = [
+    {
+      id: 1,
+      title: "팔칠댄스 ［I LOVE YOUR COMPLEX］ 부산 쇼케이스",
+      location: "부산",
+    },
+    {
+      id: 2,
+      title: "2025 비공정 단독공연 〈Hellvetica : Sabotage〉 in Busan",
+      location: "부산",
+    },
+    {
+      id: 3,
+      title: "2025 LUCY 8TH CONCERT 〈LUCID LINE〉",
+      location: "서울",
+    },
+    {
+      id: 4,
+      title: "원 오크 록 내한공연",
+      location: "서울",
+    },
+    {
+      id: 5,
+      title: "쏜애플 콘서트 ‘바다와 구름과 무대’",
+      location: "서울",
+    },
+  ];
+
+  useEffect(() => {
+    console.log(inputText);
+  }, [inputText]);
   return (
     <>
       <div className={searchStyle.inputDiv}>
         <img src={searchSVG} alt="검색" className={searchStyle.searchIcon} />
-        <input placeholder="검색어를 입력하세요" type="text" />
+        <input
+          placeholder="검색어를 입력하세요"
+          type="text"
+          value={inputText}
+          onChange={(e) => {
+            setInputText(e.target.value);
+          }}
+        />
         <img src={menuSVG} alt="메뉴" className={searchStyle.menuIcon} />
       </div>
       <div className={searchStyle.recentSearchDiv}>
@@ -39,21 +79,17 @@ export default function Search() {
       <div className={searchStyle.recommendSearchDiv}>
         <span className={searchStyle.recommendSearchHeader}>추천 검색어</span>
         <ul className={searchStyle.recommendSearchList}>
-          <li className={searchStyle.recommendSearchItem}>
-            팔칠댄스 ［I LOVE YOUR COMPLEX］ 부산 쇼케이스 - 부산
-          </li>
-          <li className={searchStyle.recommendSearchItem}>
-            2025 비공정 단독공연 〈Hellvetica : Sabotage〉 in Busan - 부산
-          </li>
-          <li className={searchStyle.recommendSearchItem}>
-            2025 LUCY 8TH CONCERT 〈LUCID LINE〉 서울
-          </li>
-          <li className={searchStyle.recommendSearchItem}>
-            원 오크 록 내한공연
-          </li>
-          <li className={searchStyle.recommendSearchItem}>
-            쏜애플 콘서트 ‘바다와 구름과 무대’
-          </li>
+          {recommendLista.map((item) => (
+            <li
+              key={item.id}
+              className={searchStyle.recommendSearchItem}
+              onClick={() => {
+                setInputText(item.title);
+              }}
+            >
+              {item.title}
+            </li>
+          ))}
         </ul>
       </div>
     </>
