a6140a4f884b89f1c3ef980863772fae3958ae8d
chominseo <singer100@yu.ac.kr>
2025-11-11 14:32:55 +0900

    feat :  Splash 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import GlobalLayout from "./layout/GlobalLayout";
 import Splash from "./pages/Splash";
 import Login from "./pages/auth/Login";
@@ -8,7 +8,7 @@ export default function App() {
  
       <Routes>
         <Route element={<GlobalLayout />}>
-          <Route index element={<Splash next="/Login" delayMs={10000000} />} />
+          <Route index element={<Splash next="/Login" delayMs={3000} />} /> {/*나중에 3초로 조정*/}
           <Route path="login" element={<Login/>}/>
         </Route>
       </Routes>
---
+++ src/pages/Splash.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import stageonlogo from ".././assets/StageonLogo.svg"; 
+import "./css/Splash.css";
+
+type SplashProps = {
+  next?: string;
+  delayMs?: number;
+};
+
+export default function Splash({
+  next = "/",
+  delayMs = 3000, /*나중에 3초로 조정 */
+}: SplashProps) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const time = setTimeout(() => navigate(next, { replace: true }), delayMs);
+    return () => clearTimeout(time);
+  }, [navigate, next, delayMs]);
+
+  return (
+    <div
+      className="splash-container"
+      role="main"
+      aria-label="Stage_On splash screen"
+    >
+   
+      <div className="headline">
+        <span>무대가</span>
+        <span>켜지는 순간의</span>
+        <span>설렘,</span>
+      </div>
+
+      <div className="brand">
+        <img src={stageonlogo} alt="StageonLogo"/>
+     
+      </div>
+    </div>
+  );
+}
