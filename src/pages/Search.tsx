488a2f7d3e70e1c31222d27c1a684cefdfa92dba
nyeonseok <aragorn2358@naver.com>
2025-11-11 13:46:54 +0900

    feat: 검색 후 카테고리 선택 별 스타일 적용

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -5,7 +5,9 @@ import xSVG from "../assets/search/x.svg";
 import { useEffect, useState } from "react";
 
 export default function Search() {
-  const [inputText, setInputText] = useState("");
+  const [inputText, setInputText] = useState<string>("");
+  const [isSearch, setIsSearch] = useState<boolean>(true);
+  const [onFestival, setOnFestival] = useState<boolean>(true);
   const recommendLista: any[] = [
     {
       id: 1,
@@ -34,6 +36,21 @@ export default function Search() {
     },
   ];
 
+  // enter key 눌렀을 때 검색 함수 호출
+  const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch(inputText);
+    }
+  };
+
+  // 검색 함수
+  const handleSearch = async (inputText: string) => {
+    // 검색 api 연동 하기
+    // if (res.status === 200) {
+    //   setIsSearch(true);
+    // }
+  };
+  // 나중에 지울거
   useEffect(() => {
     console.log(inputText);
   }, [inputText]);
@@ -48,50 +65,107 @@ export default function Search() {
           onChange={(e) => {
             setInputText(e.target.value);
           }}
+          onKeyDown={handleEnter}
         />
         <img src={menuSVG} alt="메뉴" className={searchStyle.menuIcon} />
       </div>
-      <div className={searchStyle.recentSearchDiv}>
-        <span className={searchStyle.recentSearchHeader}>최근 검색어</span>
-        <ul className={searchStyle.recentSearchList}>
-          <li className={searchStyle.recentSearchItem}>
-            쏜애플 콘서트
-            <img src={xSVG} alt="삭제" className={searchStyle.xIcon} />
-          </li>
-          <li className={searchStyle.recentSearchItem}>
-            쏜애플 콘서트
-            <img src={xSVG} alt="삭제" className={searchStyle.xIcon} />
-          </li>
-          <li className={searchStyle.recentSearchItem}>
-            쏜애플 콘서트
-            <img src={xSVG} alt="삭제" className={searchStyle.xIcon} />
-          </li>
-          <li className={searchStyle.recentSearchItem}>
-            쏜애플 콘서트
-            <img src={xSVG} alt="삭제" className={searchStyle.xIcon} />
-          </li>
-          <li className={searchStyle.recentSearchItem}>
-            쏜애플 콘서트
-            <img src={xSVG} alt="삭제" className={searchStyle.xIcon} />
-          </li>
-        </ul>
-      </div>
-      <div className={searchStyle.recommendSearchDiv}>
-        <span className={searchStyle.recommendSearchHeader}>추천 검색어</span>
-        <ul className={searchStyle.recommendSearchList}>
-          {recommendLista.map((item) => (
-            <li
-              key={item.id}
-              className={searchStyle.recommendSearchItem}
-              onClick={() => {
-                setInputText(item.title);
-              }}
-            >
-              {item.title}
-            </li>
-          ))}
-        </ul>
-      </div>
+      {isSearch == false ? (
+        <>
+          <div className={searchStyle.recentSearchDiv}>
+            <span className={searchStyle.recentSearchHeader}>최근 검색어</span>
+            <ul className={searchStyle.recentSearchList}>
+              <li className={searchStyle.recentSearchItem}>
+                쏜애플 콘서트
+                <img src={xSVG} alt="삭제" className={searchStyle.xIcon} />
+              </li>
+              <li className={searchStyle.recentSearchItem}>
+                쏜애플 콘서트
+                <img src={xSVG} alt="삭제" className={searchStyle.xIcon} />
+              </li>
+              <li className={searchStyle.recentSearchItem}>
+                쏜애플 콘서트
+                <img src={xSVG} alt="삭제" className={searchStyle.xIcon} />
+              </li>
+              <li className={searchStyle.recentSearchItem}>
+                쏜애플 콘서트
+                <img src={xSVG} alt="삭제" className={searchStyle.xIcon} />
+              </li>
+              <li className={searchStyle.recentSearchItem}>
+                쏜애플 콘서트
+                <img src={xSVG} alt="삭제" className={searchStyle.xIcon} />
+              </li>
+            </ul>
+          </div>
+          <div className={searchStyle.recommendSearchDiv}>
+            <span className={searchStyle.recommendSearchHeader}>
+              추천 검색어
+            </span>
+            <ul className={searchStyle.recommendSearchList}>
+              {recommendLista.map((item) => (
+                <li
+                  key={item.id}
+                  className={searchStyle.recommendSearchItem}
+                  onClick={() => {
+                    setInputText(item.title);
+                  }}
+                >
+                  {item.title}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={searchStyle.searchResultDiv}>
+            <div className={searchStyle.festivalAndBand}>
+              <div
+                className={searchStyle.festivalDiv}
+                onClick={() => {
+                  setOnFestival(true);
+                }}
+              >
+                <span
+                  className={`${searchStyle.festival} ${
+                    onFestival ? searchStyle.selected : searchStyle.default
+                  }`}
+                >
+                  공연
+                </span>
+                <span
+                  className={`${searchStyle.count} ${
+                    onFestival ? searchStyle.selected : searchStyle.default
+                  }     `}
+                >
+                  159
+                </span>
+              </div>
+              <div
+                className={searchStyle.bandDiv}
+                onClick={() => {
+                  setOnFestival(false);
+                }}
+              >
+                <span
+                  className={`${searchStyle.band} ${
+                    !onFestival ? searchStyle.selected : searchStyle.default
+                  }`}
+                >
+                  밴드
+                </span>
+                <span
+                  className={`${searchStyle.count} ${
+                    !onFestival ? searchStyle.selected : searchStyle.default
+                  }`}
+                >
+                  1
+                </span>
+              </div>
+            </div>
+            <div className={searchStyle.searchResultDiv}></div>
+          </div>
+        </>
+      )}
     </>
   );
 }
