fbe529fc7550afb047463deedede60d7074b4f15
nyeonseok <aragorn2358@naver.com>
2025-11-12 17:50:54 +0900

    feat: header 틀 구현, 조정 필요

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import GlobalLayout from "./layout/GlobalLayout";
 import Search from "./pages/Search";
+import HomeLayout from "./layout/HomeLayout";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<GlobalLayout />}>
-        <Route path="search" element={<Search/>}/>
+        <Route path="main" element={<HomeLayout />}></Route>
+        <Route path="search" element={<Search />} />
       </Route>
     </Routes>
   );
---
+++ src/layout/HomeLayout.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import headerStyle from "../css/components/header.module.css";
+import logoSVG from "../assets/component/header/homeLogo.svg";
+import searchSVG from "../assets/component/header/search.svg";
+import menuSVG from "../assets/component/header/menu.svg";
+
+export default function HomeLayout() {
+  return (
+    <div>
+      <header className={headerStyle.header}>
+        <img src={logoSVG} alt="로고" />
+        <input type="hidden" className={headerStyle.input} />
+        <div className={headerStyle.iconDiv}>
+          <img src={searchSVG} alt="검색" />
+          <img src={menuSVG} alt="메뉴" />
+        </div>
+      </header>
+      <Outlet />
+    </div>
+  );
+}
