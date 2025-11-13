49361ba8e49073ddd8e0e96267b51bd4dbb3ac24
nyeonseok <aragorn2358@naver.com>
2025-11-13 15:28:32 +0900

    feat: header컴포넌트화 & 메뉴바 UI

---
+++ src/components/Header.tsx
@@ -0,0 +1,57 @@
+import headerStyle from "../css/components/header.module.css";
+import logoSVG from "../assets/component/header/homeLogo.svg";
+import searchSVG from "../assets/component/header/search.svg";
+import menuSVG from "../assets/component/header/menu.svg";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+export default function Header() {
+  const navigate = useNavigate();
+  const [onMenu, setOnMenu] = useState<boolean>(false);
+  const [animateMenu, setAnimateMenu] = useState(false);
+
+  // menuDiv 생길 때 delay 주기
+  useEffect(() => {
+    if (onMenu) {
+      const timer = setTimeout(() => setAnimateMenu(true), 10);
+      return () => clearTimeout(timer);
+    } else {
+      setAnimateMenu(false);
+    }
+  }, [onMenu]);
+  return (
+    <header className={headerStyle.header}>
+      <img src={logoSVG} alt="로고" />
+      <div className={headerStyle.iconDiv}>
+        <img
+          src={searchSVG}
+          alt="검색"
+          className={headerStyle.searchIcon}
+          onClick={() => {
+            navigate("/search");
+          }}
+        />
+        <img
+          src={menuSVG}
+          alt="메뉴"
+          onClick={() => {
+            setOnMenu(true);
+          }}
+        />
+      </div>
+      {onMenu && (
+        <div
+          className={headerStyle.overlay}
+          onClick={() => setOnMenu(false)} // <- 여기를 클릭하면 메뉴 닫힘
+        >
+          <div
+            className={`${headerStyle.menuDiv} ${
+              animateMenu ? headerStyle.show : ""
+            }`}
+            onClick={(e) => e.stopPropagation()} // <- 요게 핵심
+          ></div>
+        </div>
+      )}
+    </header>
+  );
+}
--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -1,19 +1,10 @@
 import { Outlet } from "react-router-dom";
-import headerStyle from "../css/components/header.module.css";
-import logoSVG from "../assets/component/header/homeLogo.svg";
-import searchSVG from "../assets/component/header/search.svg";
-import menuSVG from "../assets/component/header/menu.svg";
+import Header from "../components/Header";
 
 export default function HomeLayout() {
   return (
     <div>
-      <header className={headerStyle.header}>
-        <img src={logoSVG} alt="로고" />
-        <div className={headerStyle.iconDiv}>
-          <img src={searchSVG} alt="검색" className={headerStyle.searchIcon} />
-          <img src={menuSVG} alt="메뉴" />
-        </div>
-      </header>
+      <Header />
       <Outlet />
     </div>
   );
