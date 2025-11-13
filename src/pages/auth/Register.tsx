15b4eb84c378f7a183341782fcb3eebfbf2835da
chominseo <singer100@yu.ac.kr>
2025-11-13 17:32:39 +0900

    feat : 회원가입 Header 구현

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,6 +1,11 @@
+import registerStyle from "../../css/pages/register.module.css";
+import vector from "../../assets/auth/Vector.svg"
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className={registerStyle.registerHeader}>
+        <img src={vector} className={registerStyle.registerVector}/>
+        <span className={registerStyle.registerText}>회원가입</span>
+    </div>
   )
 }
 
