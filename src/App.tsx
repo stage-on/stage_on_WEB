a8981c6d759616e2e988ef965f7f4ab970bf55aa
chominseo <singer100@yu.ac.kr>
2025-11-13 17:06:18 +0900

    feat : 회원가입 페이지 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import GlobalLayout from "./layout/GlobalLayout";
 import Splash from "./pages/Splash";
 import Login from "./pages/auth/Login";
 import Search from "./pages/Search";
-
+import Register from "./pages/auth/Register";
 
 export default function App() {
   return (
@@ -14,6 +14,7 @@ export default function App() {
           <Route index element={<Splash next="/Login" delayMs={3000} />} /> {/*나중에 3초로 조정*/}
           <Route path="login" element={<Login/>}/>
           <Route path="search" element={<Search/>}/>
+          <Route path="register" element={<Register/>}/>
         </Route>
       </Routes>
 
