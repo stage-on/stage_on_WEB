ca832cab06fa8fc031e3993463819b5728db1aba
nyeonseok <aragorn2358@naver.com>
2025-11-13 10:41:31 +0900

    feat: 공통 헤더 UI 구현 완료

--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -9,9 +9,8 @@ export default function HomeLayout() {
     <div>
       <header className={headerStyle.header}>
         <img src={logoSVG} alt="로고" />
-        <input type="hidden" className={headerStyle.input} />
         <div className={headerStyle.iconDiv}>
-          <img src={searchSVG} alt="검색" />
+          <img src={searchSVG} alt="검색" className={headerStyle.searchIcon} />
           <img src={menuSVG} alt="메뉴" />
         </div>
       </header>
