8666fc87846a08a6feeafb8ba6a9f52fd78f72e5
nyeonseok <aragorn2358@naver.com>
2025-11-14 13:22:22 +0900

    feat: 선택 여부, 삭제 버튼 클릭 여부에 따른 상태 관리

--- src/pages/MyBands.tsx
+++ src/pages/MyBands.tsx
@@ -7,6 +7,7 @@ export default function MyBands() {
   const bandList = [1, 2, 3, 4];
 
   const [onDelete, setOnDelete] = useState<boolean>(false);
+  const [onCheck, setOnCheck] = useState<boolean>(false);
   const [checkedList, setCheckedList] = useState<boolean[]>(
     Array(bandList.length).fill(false)
   );
@@ -17,15 +18,38 @@ export default function MyBands() {
     );
   };
 
+  useEffect(() => {
+    const trueCount = checkedList.filter((v) => v === true).length;
+    if (trueCount > 0) {
+      setOnCheck(true);
+    } else if (trueCount === 0) {
+      setOnCheck(false);
+    }
+  }, [checkedList]);
+
   return (
     <div className={myBandsStyle.container}>
       <div className={myBandsStyle.title}>MY BANDS</div>
       <div
         className={`${myBandsStyle.deleteBtn} ${
-          onDelete === true ? myBandsStyle.onDeleteBtn : ""
+          onDelete && onCheck
+            ? myBandsStyle.deleteBtn
+            : onDelete
+            ? myBandsStyle.onDeleteBtn
+            : ""
         }`}
       >
-        <span onClick={() => setOnDelete((prev) => !prev)}>선택 삭제</span>
+        {onDelete ? (
+          <span
+            onClick={() =>
+              !onCheck ? setOnDelete((prev) => !prev) : console.log("삭제로직")
+            }
+          >
+            삭제하기
+          </span>
+        ) : (
+          <span onClick={() => setOnDelete((prev) => !prev)}>선택 삭제</span>
+        )}
       </div>
 
       <div className={myBandsStyle.grid}>
@@ -33,7 +57,7 @@ export default function MyBands() {
           <div key={index} className={myBandsStyle.bandItem}>
             <div className={myBandsStyle.itemInner}>
               <div className={myBandsStyle.checkBoxDiv}>
-                {onDelete && (
+                {(onDelete || onCheck) && (
                   <img
                     src={checkedList[index] ? fullSVG : emptySVG}
                     alt="checkbox"
