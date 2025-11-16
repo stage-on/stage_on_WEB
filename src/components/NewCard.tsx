b514bd1501b112da82851af7c1542aa2d81160e3
chominseo <singer100@yu.ac.kr>
2025-11-17 00:50:42 +0900

    Merge branch 'STAGE-31-timetablepage-ui' of https://github.com/stage-on/stage_on_WEB into test

---
+++ src/components/NewCard.tsx
@@ -0,0 +1,9 @@
+import bandcardStyle from "../css/components/bandcard.module.css";
+
+const NewCard = () => {
+  return (
+   <div className={bandcardStyle.NewCard}>NEW</div>
+  )
+}
+
+export default NewCard
