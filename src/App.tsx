10c9fd919e2524d5fb4390c5bf8fbedd4e833e01
nyeonseok <aragorn2358@naver.com>
2025-11-20 15:07:57 +0900

    fix: dev안에 프로젝트 들어가있는 이슈 해결

--- src/App.tsx
+++ src/App.tsx
@@ -12,27 +12,25 @@ import TimetableMainPage from "./pages/timetable/TimetableMainPage";
 import TimetableDetailPage from "./pages/timetable/TimetableDetailPage";
 import Test from "./pages/Test";
 
-
 export default function App() {
   return (
     <Routes>
       <Route element={<GlobalLayout />}>
-      <Route path="test" element={<Test/>}/>
+        <Route path="test" element={<Test />} />
         <Route index element={<Splash next="/Login" delayMs={3000} />} />
         <Route path="login" element={<Login />} />
         <Route path="main" element={<HomeLayout />}>
           <Route path="mybands" element={<MyBands />} />
-           <Route path="home" element={<Home />} />
-           <Route path="timetable">
-          <Route index element={<TimetableMainPage />} />     
-          <Route path=":id" element={<TimetableDetailPage />} /> 
-        </Route>
+          <Route path="home" element={<Home />} />
+          <Route path="timetable">
+            <Route index element={<TimetableMainPage />} />
+            <Route path=":id" element={<TimetableDetailPage />} />
+          </Route>
         </Route>
         <Route path="search" element={<Search />} />
-        <Route path="register" element={<Register/>}/>
-         <Route path="registerfinish" element={<Registerfinish/>}/>
+        <Route path="register" element={<Register />} />
+        <Route path="registerfinish" element={<Registerfinish />} />
       </Route>
     </Routes>
-
   );
 }
