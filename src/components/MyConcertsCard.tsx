2be1c01c883bb0b31b21c0a9322290a3985ab5ed
chominseo <singer100@yu.ac.kr>
2025-11-24 00:32:51 +0900

    feat : MyConcerts 구현

--- src/components/MyConcertsCard.tsx
+++ src/components/MyConcertsCard.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import concertimage from "../assets/component/home/concert_image.svg";
+import myConcertsStyle from "../css/pages/myconcert.module.css";
+import { HiChevronRight } from "react-icons/hi2";
 
-const MyConcertsCard = () => {
-  return (
-    <div>MyConcertsCard</div>
-  )
+import heartFilled from "./../assets/timetable/heart.svg";
+import heartEmpty from "./../assets/timetable/heart-empty.svg";
+
+interface MyConcertsCardProps {
+  id: number;
+  festivalName: string;
+  location: string;
+  date: string;
+  likes: number;
+  liked: boolean;
+  onToggle: (id: number) => void;
 }
 
-export default MyConcertsCard
+const MyConcertsCard = ({
+  id,
+  festivalName,
+  location,
+  date,
+  likes,
+  liked,
+  onToggle,
+}: MyConcertsCardProps) => {
+
+  const handleLikeToggle = () => {
+    onToggle(id); 
+  };
+
+  return (
+    <>
+      <div className={myConcertsStyle.concertCardContainer}>
+        <img src={concertimage} className={myConcertsStyle.concertImg} />
+
+        <div className={myConcertsStyle.concertCardColumn}>
+          <span className={myConcertsStyle.festivalName}>{festivalName}</span>
+
+          <div className={myConcertsStyle.concertCardRow}>
+            <img
+              src={liked ? heartFilled : heartEmpty}
+              onClick={handleLikeToggle}
+            />
+            <span className={myConcertsStyle.likeText}>{likes}</span>
+            <span className={myConcertsStyle.location}>{location}</span>
+            <span className={myConcertsStyle.date}>{date}</span>
+          </div>
+        </div>
+
+        <HiChevronRight className={myConcertsStyle.Vector} />
+      </div>
+    </>
+  );
+};
+
+export default MyConcertsCard;
--- src/pages/MyConcert.tsx
+++ src/pages/MyConcert.tsx
@@ -1,14 +1,90 @@
-import React from 'react'
+import { useState } from "react";
 import myConcertsStyle from "../css/pages/myconcert.module.css";
-import MyConcertsCard from '../components/MyConcertsCard';
+import MyConcertsCard from "../components/MyConcertsCard";
 
-const MyConcert = () => {
-  return (
-    <div className={myConcertsStyle.MyConcerts}>
-    <div className={myConcertsStyle.MyConcertstitle}>My Concerts</div>
-    <MyConcertsCard/>
-    </div>
-  )
+interface Concert {
+  id: number;
+  festivalName: string;
+  location: string;
+  date: string;
+  likes: number;
+  liked: boolean;
 }
 
-export default MyConcert
+const MyConcert = () => {
+  const [concerts, setConcerts] = useState<Concert[]>([ //연동 전 더미데이터
+    {
+      id: 1,
+      festivalName: "COUNTDOWN FANTASY 2025-2026",
+      location: "일산 킨텍스",
+      date: "2025.12.20 - 12.21",
+      likes: 999,
+      liked: true,
+    },
+    {
+      id: 2,
+      festivalName: "COUNTDOWN FANTASY 2025-2026",
+      location: "일산 킨텍스",
+      date: "2025.12.20 - 12.21",
+      likes: 523,
+      liked: true,
+    },
+    {
+      id: 3,
+      festivalName: "COUNTDOWN FANTASY 2025-2026",
+      location: "일산 킨텍스",
+      date: "2025.12.20 - 12.21",
+      likes: 120,
+      liked: true,
+    },
+    {
+      id: 4,
+      festivalName: "COUNTDOWN FANTASY 2025-2026",
+      location: "일산 킨텍스",
+      date: "2025.12.20 - 12.21",
+      likes: 87,
+      liked: true,
+    },
+    {
+      id: 5,
+      festivalName: "COUNTDOWN FANTASY 2025-2026",
+      location: "일산 킨텍스",
+      date: "2025.12.20 - 12.21",
+      likes: 45,
+      liked: true,
+    },
+  ]);
+ 
+
+  // 하트 비워지면 제거
+  const handleToggle = (id: number) => {
+    setConcerts((prev) =>
+      prev
+        .map((c) =>
+          c.id === id ? { ...c, liked: !c.liked } : c
+        )
+        .filter((c) => c.liked) 
+    );
+  };
+
+  return (
+    <div className={myConcertsStyle.MyConcerts}>
+      <div className={myConcertsStyle.MyConcertstitle}>My Concerts</div>
+
+      {concerts.map((concert) => (
+        <MyConcertsCard
+          key={concert.id}
+          id={concert.id}
+          festivalName={concert.festivalName}
+          location={concert.location}
+          date={concert.date}
+          likes={concert.likes}
+          liked={concert.liked}
+          onToggle={handleToggle}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default MyConcert;
