562ca376fc28badbf618b05e9a1df6d3d865f598
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-11-24 23:39:12 +0900

    feat: callback페이지, interceptor, store구현

---
+++ src/pages/auth/Callback.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useStore } from "../../store/store";
+
+export default function Callback() {
+  const location = useLocation();
+  const login = useStore((state) => state.login);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const token = params.get("token");
+    if (token) {
+      login(token);
+      navigate("/main/home");
+    }
+  }, [location]);
+  return <></>;
+}
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,7 +1,7 @@
 import loginlogo from "../../assets/auth/LoginLogo.svg";
 import loginStyle from "../../css/pages/login.module.css";
-import googlelogo from  "../../assets/auth/Google.svg";
-import kakaologo from "../../assets/auth/KaKao.svg"
+import googlelogo from "../../assets/auth/Google.svg";
+import kakaologo from "../../assets/auth/KaKao.svg";
 
 const Login = () => {
   const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
@@ -10,38 +10,52 @@ const Login = () => {
 
   const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
   const GOOGLE_REDIRECT_URI = import.meta.env.VITE_GOOGLE_REDIRECT_URI;
-  const googlelink = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&response_type=code&scope=email%20profile`;
-
+  // const googlelink = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&response_type=code&scope=email%20profile`;
+  const googlelink = "https://stage-on.duckdns.org/oauth2/authorization/google";
   const handleGoogleLogin = () => {
     window.location.href = googlelink;
-  }
+  };
 
   const handleKaKaoLogin = () => {
     window.location.href = kakaolink;
-  }
+  };
 
   return (
-  <div className={loginStyle.fixedDiv}>
-    <img
-      src={loginlogo}
-      alt="로그인화면로고"
-      className={loginStyle.loginLogo}
-    />
-    <span className={loginStyle.loginText}>로그인</span>
+    <div className={loginStyle.fixedDiv}>
+      <img
+        src={loginlogo}
+        alt="로그인화면로고"
+        className={loginStyle.loginLogo}
+      />
+      <span className={loginStyle.loginText}>로그인</span>
 
-    <div className={loginStyle.loginButton}>
-    <button type="button"
-    onClick={handleGoogleLogin}
-    className={loginStyle.GoogleLogin}>
-    <img src={googlelogo} alt="구글로고" className={loginStyle.googleLogo}/>
-    Google 계정으로 계속</button>
+      <div className={loginStyle.loginButton}>
+        <button
+          type="button"
+          onClick={handleGoogleLogin}
+          className={loginStyle.GoogleLogin}
+        >
+          <img
+            src={googlelogo}
+            alt="구글로고"
+            className={loginStyle.googleLogo}
+          />
+          Google 계정으로 계속
+        </button>
 
-    <button type="button"
-    onClick={handleKaKaoLogin}
-    className={loginStyle.KaKaoLogin}>
-      <img src={kakaologo} alt="카카오로고" className={loginStyle.kakaoLogo}/>
-      KaKao 계정으로 계속</button>
-    </div>
+        <button
+          type="button"
+          onClick={handleKaKaoLogin}
+          className={loginStyle.KaKaoLogin}
+        >
+          <img
+            src={kakaologo}
+            alt="카카오로고"
+            className={loginStyle.kakaoLogo}
+          />
+          KaKao 계정으로 계속
+        </button>
+      </div>
     </div>
   );
 };
